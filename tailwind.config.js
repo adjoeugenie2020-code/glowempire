0b368e621ccb6c9d4f9988215c895f65fe406bba
Adjo Eugeni <adjoeugenie2020@gmail.com>
2025-11-25 10:13:25 +0100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,17 @@ export default {
   theme: {
     extend: {
       colors: {
+        /** BRANDING PRINCIPAL */
+        primary: '#003A68',
+        secondary: '#2AC5D7',
+
+        /** ANCIENNES COULEURS (tu peux les supprimer si inutiles) */
         deepBlue: '#002A5C',
         royalBlue: '#004F9E',
         gold: '#D4AF37',
         lightGray: '#F4F4F4',
       },
+
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
